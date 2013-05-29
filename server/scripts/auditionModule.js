06b322156c503a6050df2107646dab2005fcb37f
Deepak <kumarde@umich.edu>
2013-05-29 16:18:25 -0400

    fixed error message

--- server/scripts/auditionModule.js
+++ server/scripts/auditionModule.js
@@ -4,6 +4,6 @@ var db = require('mongojs').connect("db", ["votes"]);
 
 AuditionModule.prototype.UpdateDB = function(callback){
 	db.votes.update({votes:0}, { $inc: {votes:1}}, { multi: true});
-}
+};
 
 exports.AuditionModule = AuditionModule;
