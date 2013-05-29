8ed82aa672a164103d3b6c06a007062952c035a2
Deepak <kumarde@umich.edu>
2013-05-29 16:14:03 -0400

    added update function

--- server/scripts/auditionModule.js
+++ server/scripts/auditionModule.js
@@ -1,6 +1,11 @@
 AuditionModule = function(){};
 
+var db = require('mongojs').connect("db", ["votes"]);
 
-
+AuditionModule.prototype.UpdateDB = function(callback){
+	db.votes.update({votes:0}, { $inc: {votes:1}}, { multi: true});
+	if(err) callback(true, null);
+	else callback(null, users);
+}
 
 exports.AuditionModule = AuditionModule;
