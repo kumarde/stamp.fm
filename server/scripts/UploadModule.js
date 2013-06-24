55e766e57076672cacb92a451b5f4496182c2058
Deepak <kumarde@umich.edu>
2013-06-24 15:33:48 -0400

    added file type check and one entry per genre check

--- server/scripts/UploadModule.js
+++ server/scripts/UploadModule.js
@@ -1,6 +1,6 @@
 UploadModule = function(){};
 
-var db = require('mongojs').connect("stampfm", ["country", "pop", "alternative", "rap", "rnb", "instrumental", "hardrock", "EDM", "international", "folk"]);
+var db = require('mongojs').connect("stampfm", ["tournament"]);
 var moment = require('moment');
 
 UploadModule.prototype.uploadDB = function(genre, name, id, artistID)
@@ -13,6 +13,10 @@ UploadModule.prototype.uploadDB = function(genre, name, id, artistID)
 			db.genre.save({_id: id, artistID: artistID, name: name, date: moment().format('MMMM Do YYYY, h:mm:ss a')});
 		}
 	});
+}
+
+UploadModule.prototype.checkGenre = function(genre){
+
 }
 
 exports.UploadModule = UploadModule;
