5b36e63ec8b232a3e76b97e6a10f19ac9c332cba
Deepak <kumarde@umich.edu>
2013-06-27 16:29:48 -0400

    added server changes

--- server/scripts/UserModule.js
+++ server/scripts/UserModule.js
@@ -3,9 +3,9 @@ UserModule = function(){};
 var db = require('mongojs').connect("stampfm", ["profiles"])
 
 //take bio, take their name, basic information
-UserModule.prototype.updateDB = function(name, location, bio, id)
+UserModule.prototype.updateDB = function(name, location, bio, fb, twit, id)
 {
-	db.profiles.save({_id: id, name: name, location: location, bio: bio, following: [], followers: [], shared: []});
+	db.profiles.save({_id: id, name: name, location: location, bio: bio, facebook: fb, twitter: twit, following: [], followers: [], shared: []});
 }
 
 //take in a picture, and post it to S3
