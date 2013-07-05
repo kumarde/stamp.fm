8c241b7aa9ad2a36c6cc12063278ccfd9bc2e6f1
Deepak <kumarde@umich.edu>
2013-07-05 13:50:05 -0400

    didn't add anything substantial

--- server/forever.js
+++ server/forever.js
@@ -1,13 +1,23 @@
 var forever = require('forever-monitor');
 
 var child = new(forever.Monitor)('server.js',{
-	max: 3,
-	silent: true,
-	options:[{'watch': true, 'watchIgnoreDotFiles': null, 'watchIgnorePatterns': null, 'watchDirectory':'.'}] //watch = true?
+	max: 1,
+	silent: false,
+	watch: true,
+	killTree: true,
+	watchIgnoreDotFiles: null,
+	watchIgnorePatterns: null,
+	watchDirectory: __dirname,
+	options: [] //watch = true?
 });
 
 child.on('exit', function(){
 	console.log('server.js has exited after 3 restarts');
 });
+
+child.on('restart', function(){
+	console.log("restarted the server.");
+})
+
 
 child.start();
