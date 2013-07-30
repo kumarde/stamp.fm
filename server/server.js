6b5371251895897d62077b16b852d9fb6964e1a4
Jordan Weichel <weichel@umich.edu>
2013-07-30 16:32:55 -0400

    fix

--- server/server.js
+++ server/server.js
@@ -1377,3 +1377,15 @@ app.get("*", function(req, res){
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Server listening on port " + app.get('port'));
 });
+
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 10; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
