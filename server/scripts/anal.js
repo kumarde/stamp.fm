901cdb7d4ce36aa5cf93bdf3dbf7ef995f00c239
Jordan Weichel <weichel@umich.edu>
2013-08-02 16:43:19 -0400

    sdfasd

--- server/scripts/anal.js
+++ server/scripts/anal.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		cache: false,
 		success: function(data){
 			for (var i = 0; i < data.length; ++i){
-				var $div = $('<div><div>');
+				var $div = $('<div></div>');
 				var $namediv = $('<div>'+data[i].name+'<div>');
 				$div.append($namediv);
 				$('body').append($div);
