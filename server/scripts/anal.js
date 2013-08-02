20586b7ef14cd2f919e25a5f793620d0052982a0
Jordan Weichel <weichel@umich.edu>
2013-08-02 16:41:04 -0400

    aasdf13

--- server/scripts/anal.js
+++ server/scripts/anal.js
@@ -6,7 +6,9 @@ $(document).ready(function(){
 		cache: false,
 		success: function(data){
 			for (var i = 0; i < data.length; ++i){
-				var $div = $('<div>'+data[i].name+'<div>');
+				var $div = $('<div><div>');
+				var $namediv = $('<div>'+data[i].name+'<div>');
+				$div.append($namediv);
 				$('body').append($div);
 				users[data[i]._id] = $div;
 				$.ajax({
@@ -15,7 +17,7 @@ $(document).ready(function(){
 					cache: false,
 					data: {id:data[i]._id},
 					success: function(data){
-						users[data._id].append(' - '+data.name);
+						users[data._id].append('-<div>'+data.name+'</div>');
 					}
 				});
 				/*
