bdc97efd936b93c1c56e8b56483de680abea4f03
Jordan Weichel <weichel@umich.edu>
2013-08-02 16:44:47 -0400

    123

--- server/scripts/anal.js
+++ server/scripts/anal.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		success: function(data){
 			for (var i = 0; i < data.length; ++i){
 				var $div = $('<div></div>');
-				var $namediv = $('<div>'+data[i].name+'<div>');
+				var $namediv = $('<div>'+data[i].name+'</div>');
 				$div.append($namediv);
 				$('body').append($div);
 				users[data[i]._id] = $div;
